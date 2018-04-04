17c76e46d1c358921cc34ea30c8e139a5fd3f7e1
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 01:24:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
     def self.genre_count
         @@genre_count = {}
         @@genres.each do |genre|
-            @@genre_count[genre] += 1
+            @@genre_count[genre] = @@genres.count(genre)
         end
     end
 
