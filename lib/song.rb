633a2ab6160947f85d0f209f7ab20850f5cfff44
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 00:50:09 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+    # attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
+
+
+    attr_reader :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @@artists << name if !@@artists.include?(artist)
+        @@genres << genres if !@@genres.include?(genre)
+        # @@genre_count << genre
+
+        @name = name
+        @artist = artist
+        @genre = genre
+    end
+
+
+end
