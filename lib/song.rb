56c02def65c8ecf26fe7a2a8cc1f55671433cbb5
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 01:24:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     def self.artist_count
         @@artist_count = {}
         @@artists.each do |artist|
-            @@artist_count[artist] += 1
+            @@artist_count[artist] = @@artists.count(artist)
         end
     end
 
